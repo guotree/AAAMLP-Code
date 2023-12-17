c532a10c6e6b9e2429d35352ef2c8ce8bd964347
guotree <guochirui@outlook.com>
2023-12-17 18:28:04 +0800

    delete the install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-cd ~/Downloads
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-sh Miniconda3-latest-Linux-x86_64.sh
-conda env create -f environment.yml
-conda activate ml
