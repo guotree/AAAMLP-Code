b0157ccef555abd8425621c2f5f690978e00b1ed
guotree <guochirui@outlook.com>
2023-12-17 17:18:44 +0800

    add install.sh and environment.yml

---
+++ install.sh
@@ -0,0 +1,5 @@
+cd ~/Downloads
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sh Miniconda3-latest-Linux-x86_64.sh
+conda env create -f environment.yml
+conda activate ml
