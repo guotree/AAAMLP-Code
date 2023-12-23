a9f6aeb033bf97ec041e56f58c4d1a7a0e07c2d9
tree@ubuntu <guochirui@outlook.com>
2023-12-24 01:20:27 +0800

    add some hyperparameter methods

---
+++ src/chapter09/rf_grid_search.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+from sklearn import ensemble
+from sklearn import metrics
+from sklearn import model_selection
+
+if __name__ == '__main__':
+    df = pd.read_csv("../../input/mobile_train.csv")
+    X = df.drop("price_range", axis=1).values
+    y = df["price_range"].values
+    classifier = ensemble.RandomForestClassifier()
+    param_grid = {
+        "n_estimators": [100, 200, 250, 300, 400, 500],
+        "max_depth": [1, 2, 5, 7, 11, 15],
+        "criterion": ["gini", "entropy"]
+    }
+    model = model_selection.GridSearchCV(
+        estimator=classifier,
+        param_grid=param_grid,
+        scoring="accuracy",
+        verbose=10,
+        n_jobs=1,
+        cv=5
+    )
+    model.fit(X, y)
+    print("Best score: {}".format(model.best_params_))
+    print("Best parameters set:")
+    best_parameters = model.best_estimator_.get_params()
+    for param_name in sorted(param_grid.keys()):
+        print("\t{}: {}".format(param_name, best_parameters[param_name]))
